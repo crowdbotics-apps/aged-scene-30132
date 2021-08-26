3fb5db45fae88e33e0dd304ed57d1ec75d56312a
Crowdbotics <team@crowdbotics.com>
2021-08-26 19:15:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_scene_30132Identifier",
+  url: "https://aged_scene_30132Identifier.botics.co",
+  api: "https://aged_scene_30132Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
